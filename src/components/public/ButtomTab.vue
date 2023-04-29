2bf480c6e43141d499dfe044d5e1aef840eb3181
yoon <yunkh2005@naver.com>
2023-04-29 19:31:13 +0900

    2023-04-29 공통 화면 개발 - ButtomTab: 하단 탭 - PublicSearch: 검색 화면

---
+++ src/components/public/ButtomTab.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="h-screen relative">
+		<!-- nav -->
+		<div class="flex justify-center absolute bottom-0 w-full pl-5 pr-5 bg-white border-t border-gray-100">
+			<div class="w-1/5 hover:bg-gray-200 cursor-pointer">
+				<i class="fa-solid fa-list-ul text-2xl place-content-center"></i>
+				<div class="justifys-center text-xs font-bold">내역</div>
+			</div>
+			<div class="w-1/5 hover:bg-gray-200 cursor-pointer">
+				<i class="fa-solid fa-receipt text-2xl"></i>
+				<div class="text-xs font-bold">예산</div>
+			</div>
+			<div class="w-1/5 hover:bg-gray-200 cursor-pointer">
+				<i class="fa-solid fa-user-group text-2xl"></i>
+				<div class="text-xs font-bold">모금</div>
+			</div>
+			<div class="w-1/5 hover:bg-gray-200 cursor-pointer">
+				<i class="fa-solid fa-coins text-2xl"></i>
+				<div class="text-xs font-bold">미션</div>
+			</div>
+			<div class="w-1/5 hover:bg-gray-200 cursor-pointer">
+				<i class="fa-solid fa-sack-dollar text-2xl"></i>
+				<div class="text-xs font-bold">용돈</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ src/components/public/PublicSearch.vue
@@ -0,0 +1,40 @@
+<template lang="">
+    <div class=" px-4 py-2 border-b border-gray-100">
+        <!-- 뒤로가기 버튼 -->
+        <button class="mr-4">
+            <i class="fas fa-arrow-left p-3 rounded-full hover:bg-gray-100"></i>
+        </button>
+        <span class="font-extrabold text-lg">
+            화면명
+        </span>
+    </div>
+
+    <!-- 검색어 입력 창 -->
+    <div class="relative px-5 py-2">
+        <input 
+            placeholder="검색어를 입력하세요."
+            type="text" 
+            class="pl-10 rounded-full w-full p-2 bg-gray-100 text-sm focus:ring-1 focus:ring-black focus:bg-white focus:outline-none"
+        >
+        <i class="fa-solid fa-magnifying-glass absolute left-0 mt-2 ml-8 text-sm text-light"></i>
+    </div>
+
+    <!-- 선택 버튼 -->
+    <div class="flex px-20 py-2 items-center">
+        <div class="mr-4 text-center w-1/2 ring-1 ring-gray-300 hover:bg-gray-300 text-lg font-semibold rounded-md">
+            선택1
+        </div>
+        <div class="text-center w-1/2 ring-1 ring-gray-300 hover:bg-gray-300 text-lg font-semibold rounded-md">
+            선택2
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
