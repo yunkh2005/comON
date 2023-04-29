c1df74f0ec8ad3175f335c37d70115f9b97f1c1b
yoon <yunkh2005@naver.com>
2023-04-29 19:28:17 +0900

    2023-04-29 init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<ButtomTab></ButtomTab>
+</template>
+<script>
+import ButtomTab from './components/public/ButtomTab.vue';
+
+export default {
+	components: { ButtomTab, }
+}
+</script>
+<style></style>
