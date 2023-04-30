f8170973aee8ae860644c5053e0f25b4b7982514
yoon <yunkh2005@naver.com>
2023-04-30 23:44:42 +0900

    2023-04-30 공통 팝업 수정

--- src/components/public/PublicModal.vue
+++ src/components/public/PublicModal.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex justify-center min-h-screen pt-52 px-5 pb-52 text-center">
+    <div class="fixed w-screen h-screen overflow-y-auto">
+        <div class="flex justify-center px-10 py-60 text-center">
             <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                <!-- 뒷 배경 -->
                 <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
 
             <!-- 내용 -->
-            <div class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all my-8 align-middle max-w-lg w-full"
+            <div class="inline-block overflow-hidden shadow-xl transform transition-all align-middle max-w-lg w-full bg-white rounded-lg text-left"
                 role="dialog" 
                 aria-modal="true" 
                 aria-labelledby="modal-headline">
@@ -16,12 +17,12 @@
                 </div>
 
                 <!-- 안내 멘트 -->
-                <div class="justify-center text-center text-lg font-extrabold py-8">
+                <div class="justify-center text-center text-lg font-extrabold py-10">
                         어떤 팝업을 정의하십니까?
                 </div>
 
                 <!-- 버튼 -->
-                <div class="flex place-content-center px-5 py-10 cursor-pointer">
+                <div class="flex place-content-center p-10 cursor-pointer">
                     <button class="w-1/2 p-4 text-center ring-1 ring-gray-300 hover:bg-gray-300 text-lg font-semibold rounded-lg">
                         선택1
                     </button>
