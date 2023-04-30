d39c6f1143218818e393b5f4f535a445e55cc6f8
yoon <yunkh2005@naver.com>
2023-05-01 00:38:09 +0900

    2023-05-01 공통 하단 버튼 생성 - PublicButton.vue: 메인화면에서 사용하는 우측하단 버튼 2개

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-	<ButtomTab></ButtomTab>
 	<AllowanceMain></AllowanceMain>
+	<PublicButton></PublicButton>
 	<ButtomTab></ButtomTab>
 </template>
 <script>
 // import PublicSearch from './components/public/PublicSearch.vue';
-// import PublicSearch from './components/public/PublicSearch.vue';
 import ButtomTab from './components/public/ButtomTab.vue';
 import AllowanceMain from './pages/allowance/AllowaceMain.vue'
+import PublicButton from './components/public/PublicButton.vue';
 
 export default {
-	components: { ButtomTab, AllowanceMain }
+	components: { 
+		ButtomTab, 
+		AllowanceMain,
+		PublicButton,
+	}
 }
 </script>
 <style></style>
