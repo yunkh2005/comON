6d3f35093680f4811be2c830b90cf1bf485de4dc
yoon <yunkh2005@naver.com>
2023-05-01 18:34:29 +0900

    2023-05-01 화면 수정 - PublicSearch.vue: 모바일 화면에서 버튼 수정 - PublicSelect.vue: 메인 화면에서 쓸 상단 버튼 화면

--- src/components/public/PublicSearch.vue
+++ src/components/public/PublicSearch.vue
@@ -21,7 +21,7 @@
     </div>
 
     <!-- 선택 버튼 -->
-    <div class="flex items-center px-60 cursor-pointer">
+    <div class="flex items-center w-full px-24 cursor-pointer">
         <div class="text-center w-1/2 ring-1 ring-gray-300 hover:bg-gray-300 text-lg font-semibold rounded-l-lg">
             선택1
         </div>
--- src/components/public/PublicSelect.vue
+++ src/components/public/PublicSelect.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex items-center px-60 cursor-pointer">
-        <div class="text-center w-1/2 ring-1 ring-gray-300 hover:bg-gray-300 text-lg font-semibold rounded-l-lg">
+    <div class="flex items-center w-full px-24 py-6 cursor-pointer">
+        <div class="text-center w-1/2 h-8 ring-1 ring-gray-300 hover:bg-gray-300 text-xl font-semibold rounded-l-lg">
             선택1
         </div>
-        <div class="text-center w-1/2 ring-1 ring-gray-300 hover:bg-gray-300 text-lg font-semibold rounded-r-lg">
+        <div class="text-center w-1/2 h-8 ring-1 ring-gray-300 hover:bg-gray-300 text-xl font-semibold rounded-r-lg">
             선택2
         </div>
     </div>
