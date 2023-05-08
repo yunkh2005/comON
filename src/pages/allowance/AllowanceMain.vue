c9e3f4db79d79a711f8ca07c4c6de3373c5f237f
yoon <yunkh2005@naver.com>
2023-05-08 21:50:44 +0900

    2023-05-08 용돈 화면 생성 - AllowanceMain.vue: 용돈 메인 화면 생성 - MoneyAllowance.vue: 용돈 조르기 화면 생성

---
+++ src/pages/allowance/AllowanceMain.vue
@@ -0,0 +1,134 @@
+<template lang="">
+    <div class="flex flex-col">
+        <!-- 월 이동 버튼 -->
+        <div class="flex absolute bg-white w-full mt-6 py-1">
+            <!-- 이전 월 이동 버튼 -->
+            <button class="w-1/3">
+                <i class="fa-solid fa-chevron-left px-2 text-xl hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+            </button>
+            <!-- 월 안내 -->
+            <button class="w-1/3 font-extrabold text-2xl text-center hover:bg-skyblue hover:bg-opacity-20 rounded-full">
+                <span>MM</span>
+                <span>월</span>
+            </button>
+            <!-- 이후 월 이동 버튼 -->
+            <button class="w-1/3 ">
+                <i class="fa-solid fa-chevron-right px-2 text-xl hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+            </button>
+        </div>
+
+        <!-- 용돈 총 금액-->
+        <div class="pt-20 mx-5">
+            <div class="flex justify-between px-3 py-1 bg-skyblue rounded-lg text-2xl text-white">
+                <div class="font-bold text-center">
+                    <div>받은</div>
+                    <div>용돈</div>
+                </div>
+                <div class="self-center">
+                    <span>
+                        100,000,000,000,000
+                    </span>
+                    <span class="font-bold">
+                        원
+                    </span>
+                </div>
+            </div>
+            <div class="flex justify-between mt-1 px-3 py-1 bg-skyblue rounded-lg text-2xl text-white">
+                <div class="font-bold text-center">
+                    <div>준</div>
+                    <div>용돈</div>
+                </div>
+                <div class="self-center text-gray-100">
+                    <span>
+                        100,000,000,000,000
+                    </span>
+                    <span class="font-bold">
+                        원
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <!-- 받은 메시지 -->
+        <div class="flex">
+            <div class="mx-5 my-4 py-1 justify-start text-2xl font-bold">
+                받은 메시지
+            </div>
+            <!-- 메시지 정보 -->
+            <div class="absolute h-60 w-full mt-14 overflow-y-auto">
+                <div class="flex justify-between mx-5 my-1 py-4 bg-gray-100 rounded-lg" v-for="message in 100" :key="message">
+                    <!-- 메시지 첨부 사진 -->
+                    <div class="w-1/4 self-center pl-3 text-4xl text-center">
+                        <i class="fa-solid fa-users-viewfinder"></i>
+                    </div>
+                    <div class="w-3/5 self-center">
+                        <div class="h-full text-xl font-bold">
+                            <span>발신자:</span>
+                            <span class="pl-1">가족 이름</span>
+                        </div>
+                        <div class="w-full">
+                            메시지 내용 한줄 미리보기
+                        </div>
+                    </div>
+                    <button class="w-1/10 self-center pr-3 text-xl">
+                        <i class="px-2 py-1 fa-solid fa-chevron-right hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- 보낸 메시지 -->
+        <div class="flex mt-56">
+            <div class="flex-col mx-5 my-4 py-1 justify-start text-2xl font-bold">
+                보낸 메시지
+            </div>
+            <div class="absolute h-60 w-full mt-14 overflow-y-auto">
+                <div class="flex justify-between mx-5 my-1 py-4 bg-gray-100 rounded-lg" v-for="message in 100" :key="message">
+                    <!-- 메시지 첨부 사진 -->
+                    <div class="w-1/4 self-center pl-3 text-4xl text-center">
+                        <i class="fa-solid fa-users-viewfinder"></i>
+                    </div>
+                    <div class="w-3/5 self-center">
+                        <div class="h-full text-xl font-bold">
+                            <span>수신자:</span>
+                            <span class="pl-1">가족 이름</span>
+                        </div>
+                        <div class="w-full">
+                            메시지 내용 한줄 미리보기
+                        </div>
+                    </div>
+                    <button class="w-1/10 self-center pr-3 text-xl">
+                        <i class="px-2 py-1 fa-solid fa-chevron-right hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+                    </button>
+                </div>  
+            </div>
+        </div>
+    </div>
+
+    <!-- 하단 우측 버튼 -->
+    <div class="fixed bottom-0 right-0 pb-20 justify-items-end">
+        <!-- 조르기 전체 기록 버튼 -->
+        <div class="flex flex-row cursor-pointer place-content-end p-3">
+            <i class="px-3 py-2 fa-solid fa-comments bg-skyblue hover:bg-opacity-50 rounded-full text-2xl text-white"></i>
+        </div>
+        <!-- 조르기 버튼 -->
+        <div class="flex flex-row cursor-pointer place-content-end p-3">
+            <i class="px-3 py-2 fa-solid fa-comment-dollar bg-mainblue hover:bg-opacity-50 rounded-full text-3xl text-white"></i>
+        </div>
+    </div>
+
+    <!-- 하단탭 -->
+    <ButtomTab></ButtomTab>
+</template>
+<script>
+import ButtomTab from '../../components/public/ButtomTab.vue';
+
+export default {
+    components: {
+        ButtomTab,
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/pages/allowance/MoneyAllowance.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- 상단 -->
+    <div class="flex p-2 items-center">
+        <!-- 닫기 버튼 -->
+        <button class="fa-solid fa-arrow-left text-primary text-xl p-2 h-12 w-12 hover:bg-skyblue hover:bg-opacity-20 rounded-full"></button>
+        <!-- 화면명 -->
+        <div class="flex-col pl-4 font-bold text-2xl">
+            용돈 조르기
+        </div>
+    </div>
+
+    <!-- 중앙 내용 -->
+    <div class="absolute inset-0 mt-20 mb-10 overflow-y-auto">
+        <div class="flex justify-between h-1/6 mx-5 my-1 py-4 bg-gray-100 rounded-lg" v-for="message in 20" :key="message">
+            <!-- 모금 설명 사진 -->
+            <div class="w-2/5 self-center text-6xl text-center">
+                <i class="fa-solid fa-circle-user"></i>
+            </div>
+            <div class="w-3/5 self-center">
+                <!-- 가족 이름 -->
+                <div class="text-2xl font-bold">
+                    가족 이름
+                </div>
+            </div>
+            <button class="w-1/10 self-center pr-3 text-xl">
+                <i class="px-2 py-1 fa-solid fa-chevron-right hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style></style>
