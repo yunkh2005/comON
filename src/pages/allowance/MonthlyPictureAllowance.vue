7c815b338f5d80278f7bca406aa7abaf5dce41ee
Sooin Lee <leesooin0111@gmail.com>
2023-05-15 12:57:02 +0900

    Revert "Revert "no message""
    This reverts commit 98a1287ce187212bafcd90109ef31b7b78b832fd.

---
+++ src/pages/allowance/MonthlyPictureAllowance.vue
@@ -0,0 +1,58 @@
+<template>
+        <!-- 상단 -->
+        <div class="flex p-2 items-center">
+        <!-- 닫기 버튼 -->
+        <button
+            class="fa-solid fa-arrow-left text-primary text-xl p-2 h-12 w-12 hover:bg-skyblue hover:bg-opacity-20 rounded-full">
+        </button>
+        <!-- 화면명 -->
+            <div class="flex-col pl-4 font-bold text-2xl">
+                월별 사진
+            </div>
+        </div>
+    
+        <div class="flex absolute bg-white w-full mt-6 py-1">
+            <!-- 이전 월 이동 버튼 -->
+            <button class="w-1/3">
+                <i class="fa-solid fa-chevron-left px-2 text-xl hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+            </button>
+            <!-- 월 안내 -->
+            <button class="w-1/3 font-extrabold text-2xl text-center hover:bg-skyblue hover:bg-opacity-20 rounded-full">
+                <span>MM</span>
+                <span>월</span>
+            </button>
+            <!-- 이후 월 이동 버튼 -->
+            <button class="w-1/3 ">
+                <i class="fa-solid fa-chevron-right px-2 text-xl hover:bg-skyblue hover:bg-opacity-20 rounded-full"></i>
+            </button>
+        </div>
+    <!-- 중앙 내용 -->
+    <div class="absolute inset-0 mx-5 mt-32 mb-20 overflow-y-auto bg-gray-100 text-center">
+        <!-- 안내 문구 -->
+        <div class="flex-col place-self-center text-2xl font-bold">
+            <div class="w-10 h-10 hover:opacity-50 cursor-pointer">
+                <i class="fa-solid fa-image"></i>
+            </div>
+        </div>
+        <div class="flex-col place-self-center text-2xl font-bold">
+            <div class="w-10 h-10 hover:opacity-50 cursor-pointer">
+                <i class="fa-solid fa-image"></i>
+            </div>
+        </div>
+        
+    </div>
+    <!-- 하단 탭 -->
+    <ButtomTab></ButtomTab>
+</template>
+<script>
+import ButtomTab from '../../components/public/ButtomTab.vue';
+
+export default {
+    components: { 
+		ButtomTab,
+	}
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/pages/allowance/RequestAllowanceModal.vue
@@ -0,0 +1,89 @@
+<template>
+    <div aria-labelledby="modal-title" role="dialog" aria-modal="true" @click="$emit('close-modal')">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div @click.stop class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div class="inline-block overflow-hidden shadow-xl transform transition-all align-middle max-w-lg w-full bg-white rounded-lg text-left"
+                        role="dialog" 
+                        aria-modal="true" 
+                        aria-labelledby="modal-headline">
+                        <div v-if="!enterYn">
+                            <!-- 닫기 버튼 -->
+                            <button @click="$emit('close-modal')" class="fas fa-times float-right text-primary text-xl p-2 h-10 w-10 hover:bg-gray-100 rounded-full"></button>
+                            <!-- 안내 멘트 -->
+                            <div class="flex justify-center font-extrabold py-10">
+                                <div class="mr-3 text-lg">받는 이:</div>
+                                <select class="w-1/3 rounded-md ring-1 ring-inset ring-gray-700 focus:ring-mainblue text-sm">
+                                    <option>이름</option>
+                                    <option>이름</option>
+                                    <option>이름</option>
+                                </select>
+                            </div>
+
+                            <!-- 메시지 입력-->
+                            <div>
+                                <div class="flex-1 flex justify-center text-2xl mb-9 ">
+                                    <textarea v-model="allowanceMessage" rows="10"
+                                        class="w-5/6 rounded ring-1 ring-gray-700 text-sm resize-none"
+                                        placeholder=" 메시지를 입력하세요">
+                                    </textarea>
+                                    
+                                </div>
+                                <!-- 사진 버튼-->
+                                <div class="flex justify-center">
+                                    <button
+                                        class="w-1/8 ml-2 p-2 text-center bg-mainyellow hover:text-white text-s font-semibold rounded-lg">
+                                        사진추가 <i class="fa-solid fa-image"></i>
+                                    </button>
+                                </div>
+                            </div>
+                           
+                           
+                            <!-- 버튼 -->
+                            <!-- 버튼 -->
+                            <div class="flex place-content-center p-10">
+                                <button @click="$emit('close-modal')"
+                                    class="w-1/2 ml-5 p-4 text-center ring-1 ring-gray-100 bg-gray-100 hover:bg-skyblue hover:text-white text-lg font-semibold rounded-lg">
+                                    취소
+                                </button>
+                                <button @click="allowanceMessage ? enterYn = true : enterYn = false"
+                                    class="w-1/2 ml-5 p-4 text-center ring-1 ring-skyblue bg-skyblue hover:opacity-70 text-white text-lg font-semibold rounded-lg">
+                                    전송
+                                </button>
+                            </div>
+                        </div>
+                        <div v-else class="flex flex-col">
+                            <button class="fas fa-times text-primary text-xl p-2 h-10 w-10 hover:bg-gray-100 rounded-full"></button>
+                            <div class="text-center text-lg font-extrabold py-10">
+                                메시지를 전송했습니다
+                                <div class="flex justify-center p-10">
+                                    <i class="fa-light fa-hands-clapping"></i>
+                                </div>
+                            </div>
+                            
+                            <button @click="$emit('close-modal')" class="w-1/2 m-auto mb-4 p-4 bg-skyblue hover:opacity-70 text-white text-lg font-semibold rounded-lg">
+                                확인
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            enterYn: false,
+            fundMessage: "",
+        }
+    },
+
+}
+</script>
+
+<style></style>
