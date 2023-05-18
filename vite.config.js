012ab7b9c9411c1a4604293b3e3a5a1b33ab0b24
jujube12 <gyubam1@gmail.com>
2023-05-19 01:32:27 +0900

    2023-05-19 스프링부트 빌드 경로 연결

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "../comOn-spring/src/main/resources/static",
+  }, // 빌드 결과물이 생성되는 경로
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    }, // proxy 설정
+  },
 })
